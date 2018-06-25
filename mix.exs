792f9c5ee2d01dbacb7c403f8d5778e072b20c6c
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-25 16:02:40 +0900

    hex publish

--- mix.exs
+++ mix.exs
@@ -1,28 +1,42 @@
 defmodule Memkash.MixProject do
   use Mix.Project
 
-  def project do
+  def project() do
     [
       app: :memkash,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  defp description() do
+    "Memcached Client Library for Elixir"
+  end
+
+  defp package() do
+    [
+      maintainers: ["kdxu"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kdxu/memkash"}
+    ]
+  end
+
+  def application() do
     [
       extra_applications: [:logger, :connection]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
       {:connection, "~> 1.0.4"},
-      {:poolboy, "~> 1.5.1"}
+      {:poolboy, "~> 1.5.1"},
+      {:ex_doc, "~> 0.18.3", only: [:dev], runtime: false},
     ]
   end
 end
